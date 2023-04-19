bfb91c902ac2586128c4bcaa16e21e79c28af13e
Andre Jendrysseck <ajendry@gwdg.de>
2023-04-19 22:39:20 +0200

    first step

--- gardenalocalcontrol.py
+++ gardenalocalcontrol.py
@@ -9,3 +9,28 @@ from config import MQTT_PASSWORD
 from config import MQTT_TOPIC_SUBSCRIBE
 from config import MQTT_TOPIC_PUBLISH
 from config import SCRIPT_VERSION
+
+import logging
+import threading
+import time
+import json
+from pynng import Sub0
+
+def gardenaCommandBuilder(command, device, meta):
+    gardenaCommand = ""
+    return gardenaCommand
+
+def gardenaEventInterpreter(event_str):
+    try:
+        gardenaEventDict = json.loads(event_str)
+    except:
+        pass
+
+def gardenaEventSubscribeTask(name):
+    logging.debug("gardenaEventSubscribe Task is starting reading: ", name)
+    with Sub0(dial='ipc:///tmp/lemonbeatd-event.ipc') as sub0:
+        sub0.subscribe("")
+        gardenaEventInterpreter(sub0.recv().decode('utf-8'))
+
+if __name__ == "__main__":
+    
