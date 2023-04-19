b9dd559d0d81b0bd2a06292fb578b8be37857ccf
F B <130887935+FredBit90@users.noreply.github.com>
2023-04-19 21:19:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ MQTT_AUTHENTICATION = False                                     # Mqtt authentic
 MQTT_USER = ''			                                        # Mqtt User name
 MQTT_PASSWORD = ''			                                    # Mqtt password
 MQTT_TOPIC_BASE = "GatewayName/DeviceAddress/"                  # Base topic
-MQTT_TOPIC_SUBSCRIBE = MQTT_TOPIC_BASE + 'Value'		        # Subscribe topic
+MQTT_TOPIC_SUBSCRIBE = MQTT_TOPIC_BASE          		        # Subscribe topic
 MQTT_TOPIC_PUBLISH = MQTT_TOPIC_BASE + 'Command'                # Command Topic
 SCRIPT_VERSION = '1.0.0.0'					                    # Version of GardenaLocalControl script
