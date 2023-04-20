78b3d6c34258f4b834e5ead36110986be42aa4b6
FredBit90 <0690fred@gmail.com>
2023-04-20 22:12:39 +0200

    Optimize PublishEventData. Integrate SubCmdData

--- gardenalocalcontrol.py
+++ gardenalocalcontrol.py
@@ -16,9 +16,11 @@ from config import MQTT_BROKER_PORT
 from config import MQTT_AUTHENTICATION
 from config import MQTT_BROKER_USER
 from config import MQTT_BROKER_PASSWORD
+from config import MQTT_CLIENT_ID_BASE
 from config import MQTT_TOPIC_SUBSCRIBE
 from config import MQTT_TOPIC_PUBLISH
 from config import GARDENA_NNG_FORWARD_PATH
+from config import MQTT_PUBLISH_RETAIN
 from config import SCRIPT_VERSION
 
 
@@ -45,139 +47,214 @@ def gardenaEventSubscribeTask():
 
 #Class to store nng EventData
 class EventData:
-    def __init__(self,eventtype, eventvalue):
+    def __init__(self, deviceAddress, eventtype, eventvalue):
+        self.deviceAddress = deviceAddress
         self.eventtype = eventtype
         self.eventvalue = eventvalue
 
+#Class to store nng commandData
+class CommandData:
+    def __init__(self, deviceAddress, eventtype, eventvalue):
+        self.deviceAddress = deviceAddress
+        self.eventtype = eventtype
+        self.eventvalue = eventvalue    
 
+#Class to store all data to communicate with mqtt
+class MQTTClientData:
+        def __init__(self, connectionReturnCode, disconnectionReturnCode):
+            self.connectionReturnCode = connectionReturnCode
+            self.disconnectionReturnCode = disconnectionReturnCode
 
 
 #Queue for publish events
-publishQueue = Queue()
+publishEventDataQueue = Queue()
+#Queue for subscribe commands
+subscribeCommandDataQueue = Queue()
+#List for all mqtt clients
+mqttClientDict = dict()
 
 #Connect callback for MQTT clients
 def connectCallback(client, userdata, flags, rc):
-    global mqttConnectionReturnCode_M
-    global mqttDisconnectionReturnCode_M
+    mqttClientData = mqttClientDict.get(client)
+    if mqttClientData is None:
+        logging.debug("MQTT client not found")
+        return
     #Reset disconnection code
-    mqttDisconnectionReturnCode_M = -1
-    mqttConnectionReturnCode_M = rc
+    mqttClientData.disconnectionReturnCode = -1
+    mqttClientData.connectionReturnCode = rc
     if rc==0:
-        print("MQTT connected OK returned code=",rc)
+        logging.debug("MQTT connected OK returned code=%s",rc)
     else:
-        print("MQTT bad disconnection returned code=",rc)
+        logging.debug("MQTT bad disconnection returned code=%s",rc)
 #def connectCallback(client, userdata, flags, rc):
         
 #Disconnect callback for MQTT clients
 def disconnectCallback(client, userdata, rc):
-    global mqttDisconnectionReturnCode_M
-    global mqttConnectionReturnCode_M
+    mqttClientData = mqttClientDict.get(client)
+    if mqttClientData is None:
+        logging.debug("MQTT client not found")
+        return
     #Reset connection code
-    mqttConnectionReturnCode_M = -1
-    mqttDisconnectionReturnCode_M = rc
+    mqttClientData.connectionReturnCode = -1
+    mqttClientData.disconnectionReturnCode = rc
     if rc==0:
-        print("MQTT disconnected OK returned code=",rc)
+        logging.debug("MQTT disconnected OK returned code=%s",rc)
     else:
-        print("MQTT bad disconnection returned code=",rc)
+        logging.debug("MQTT bad disconnection returned code=%s",rc)
 #def disconnectCallback(client, userdata, flags, rc):
-     
-#Methode um eine Verbindung zu der übergebenen Broker Adresse herzustellen und
-#zu warten, bis die Verbindung hergestellt wurde
-#client: MQTT Client Objekt mit dem die Verbindung hergestellt werden soll
-#brokerAddress: Adresse des MQTT Brokers, zu dem eine Verbindung hergestellt werden soll
+
+#Connect callback for subscribe command data
+def connectSubscribeCommandDataCallback(client, userdata, flags, rc):
+    client.subscribe(MQTT_TOPIC_SUBSCRIBE.format("#"))
+#def connectSubscribeCommandDataCallback(client, userdata, flags, rc):
+
+#callback with received command data
+def subscribeCommandDataCallback(client, userdata, msg):
+    #TODO parse received messages to CommandData
+    logging.debug(msg.topic + ": " + str(msg.payload))
+#def def subscribeCommandDataCallback(client, userdata, msg):
+
+#method to establish a connection to the given broker address and to wait
+#wait until the connection is established
+#client: MQTT client object with which the connection should be established
+#brokerAddress: address of the MQTT broker to connect to
 def connectMQTTBrokerAndWait(client, brokerAddress): 
-    #Verbindung zum Broker herstellen
+    #Connect to the broker
     client.connect(brokerAddress)
-    #Solange warten, bis das Connection Event aufgerufen wurde
-    waitForMQTTConnect()
+    #Wait until the connection event has been called.
+    waitForMQTTConnect(client)
 #def connectMQTTBrokerAndWait(client, brokerAddress):
 
-#Methode um zu warten, bis die Verbindung zum MQTT Broker hergestellt wurde
-def waitForMQTTConnect():
-    global mqttConnectionReturnCode_M
-    #Solange warten, bis das Connection Event aufgerufen wurde
-    while mqttConnectionReturnCode_M == -1:
-        print("MQTT in connect wait loop")
-        time.sleep(1)
+#Method to wait until the connection to the MQTT broker is established.
+def waitForMQTTConnect(client):
+    mqttClientData = mqttClientDict.get(client)
+    if mqttClientData is None:
+        logging.debug("MQTT client not found")
+        return
+    #Wait until the connection event has been called.
+    while mqttClientData.connectionReturnCode == -1:
+        #logging.debug("MQTT in connect wait loop")
+        time.sleep( 0.0001 )
 #def waitForMQTTConnect():
 
-#Methode um die Verbindung zum MQTT Broker zu trennen und zu warten bis die Verbindung getrennt wurde
-#client: MQTT Client der die Verbindung trennen soll
+#method to disconnect the MQTT broker and wait until the connection is disconnected
+#client: MQTT client which should disconnect the connection
 def disconnectMQTTBrokerAndWait(client): 
-    #Verbindung zum Broker trennen
+    #Disconnect from the broker
     client.disconnect()
-    #Solange warten, bis das Disconnect Event aufgerufen wurde
-    waitForMQTTDisconnect()
+#Wait until the disconnect event has been called
+    waitForMQTTDisconnect(client)
 #def disconnectMQTTBrokerAndWait(client):
     
-#Methode um zu warten, bis die Verbindung zum MQTT Broker hergestellt wurde
-def waitForMQTTDisconnect():
-    global mqttDisconnectionReturnCode_M
-    #Solange warten,bis das Disconnection Event aufgerufen wurde
-    while mqttDisconnectionReturnCode_M == -1:
-        print("MQTT in disconnect wait loop")
-        time.sleep(1)
-#def waitForMQTTConnect():
+#Method to wait until the connection to the MQTT broker is established.
+def waitForMQTTDisconnect(client):
+    mqttClientData = mqttClientDict.get(client)
+    if mqttClientData is None:
+        logging.debug("MQTT client not found")
+        return
+    #Wait until the disconnection event has been called.
+    while mqttClientData.disconnectionReturnCode == -1:
+        #logging.debug("MQTT in disconnect wait loop")
+        time.sleep( 0.0001 )
+#def waitForMQTTDisconnect():
 
-#Methode um die übergebenen Daten an den MQTT Broker zu senden
-#client: MQTT Client der die Daten veröffentlichen soll
-#clientName: Name des Clients
+#method to send the passed data to the MQTT broker
+#client: MQTT client which should publish the data
+#clientName: name of the client
+#dataName: name of datavalue to publish
+#dataValue: Datavalue
 def publishMQTTData(client, clientName, dataName, dataValue):
-    global mqttDisconnectionReturnCode_M
-    global mqttConnectionReturnCode_M
-    #Nur ausführen, wenn die Verbindung nicht getrennt wurde und eine Verbindung besteht
-    if mqttDisconnectionReturnCode_M == -1 and mqttConnectionReturnCode_M == 0:
-        #Nachricht veröffentlichen
+    mqttClientData = mqttClientDict.get(client)
+    if mqttClientData is None:
+        logging.debug("MQTT client not found")
+        return
+    #Execute only if the connection has not been disconnected and a connection exists
+    if mqttClientData.disconnectionReturnCode == -1 and mqttClientData.connectionReturnCode == 0:
+        #Publish message
         returnValue = client.publish(str(clientName) + "/" + str(dataName), dataValue, qos=0, retain=MQTT_PUBLISH_RETAIN)
-        print("MQTT Wait for publish")
-        #Warten bis die Nachricht veröffentlicht wurde oder die Verbindung getrennt wurde
-        while not returnValue.is_published and mqttDisconnectionReturnCode_M == -1:
-            print("MQTT in publish wait loop")
-            time.sleep(1)
+        logging.debug("MQTT Wait for publish")
+        #Wait until the message has been published or the connection has been disconnected
+        while not returnValue.is_published and mqttClientData.disconnectionReturnCode == -1:
+            #logging.debug("MQTT in publish wait loop")
+            time.sleep( 0.0001 )
 #def publishMQTTData(client, clientName, dataName, dataValue):
 
-#Method to send mqtt data
-def sendMQTTData():
-    global mqttConnectionReturnCode_M
-    global mqttDisconnectionReturnCode_M
+#method to send the passed data to the MQTT broker
+#client: MQTT client which should publish the data
+#clientName: name of the client
+#eventData: Event data to publish
+def publishMQTTData(client, clientName, eventData):
+    mqttClientData = mqttClientDict.get(client)
+    if mqttClientData is None:
+        logging.debug("MQTT client not found")
+        return
+    #Execute only if the connection has not been disconnected and a connection exists
+    if mqttClientData.disconnectionReturnCode == -1 and mqttClientData.connectionReturnCode == 0:
+        #Publish message
+        returnValue = client.publish(clientName.format(eventData.deviceAddress) + "/" + str(eventData.eventtype), str(eventData.eventvalue), qos=0, retain=MQTT_PUBLISH_RETAIN)
+        logging.debug("MQTT Wait for publish")
+        #Wait until the message has been published or the connection has been disconnected
+        while not returnValue.is_published and mqttClientData.disconnectionReturnCode == -1:
+            #logging.debug("MQTT in publish wait loop")
+            time.sleep( 0.0001 )
+#def publishMQTTData(client, clientName, dataName, dataValue):
+
+#Method to send event data to mqtt
+def publishEventDataToMQTT():
     while True:
-        #Wenn keine Elemente in der Queue sind Schleifendurchlauf abbrechen
-        if publishQueue.empty():
+        #There must be a message in the queue
+        if publishEventDataQueue.empty():
             continue
-        mqttConnectionReturnCode_M = -1
-        mqttDisconnectionReturnCode_M = -1
-        #MQTT Client Objekt erstellen
-        client = mqtt.Client(MQTT_TOPIC_SUBSCRIBE)
-        #Connect Callback zuweisen
+        #Create MQTT client object and add to dict
+        client = mqtt.Client(MQTT_CLIENT_ID_BASE + "_PublishEventData")
+        mqttClientDict.update({client: MQTTClientData(-1, -1)})
+        mqttClientData = mqttClientDict[client]
+        #Set events
         client.on_connect = connectCallback
-        #Disconnect Callback zuweisen
         client.on_disconnect = disconnectCallback
+        #Set username and pasword if authentication is required
         if MQTT_AUTHENTICATION:
-            #Benutzername und Passwort setzen
             client.username_pw_set(username=MQTT_BROKER_USER,password=MQTT_BROKER_PASSWORD)
         try:
             client.loop_start()
-            #Verbindung zum MQTT Broker herstellen
+            #Connect to the MQTT broker
             connectMQTTBrokerAndWait(client, MQTT_BROKER_IP)
-            #Wenn Connection erfolgreich ausgeführt wurde ausführen
-            if mqttConnectionReturnCode_M == 0:
-                #Alle Einträge aus der Queue übermitteln
-                while publishQueue.qsize() > 0:
+            #Execute if connection was successfully
+            if mqttClientData.connectionReturnCode == 0:
+                #Transmit all entries from the queue
+                while publishEventDataQueue.qsize() > 0:
                     # Get item
-                    item = publishQueue.get()
-                    # check for stop
+                    item = publishEventDataQueue.get()
+                    #Only publish valid items
                     if item is None:
                         continue
-                    publishMQTTData(client, MQTT_TOPIC_PUBLISH, item.eventtype, item.eventvalue)
-                #Verbindung zum MQTT Broker trennen
+                    publishMQTTData(client, MQTT_TOPIC_PUBLISH, item)
+                    #Disconnect from MQTT broker           
                 disconnectMQTTBrokerAndWait(client)
-        except:
+        except Exception as e:
             client.disconnect()
-            print("MQTT Exception")
+            logging.exception(e)
         finally:
             client.loop_stop()
-        #TODO Thread abgeben?
-#def sendMQTTData():
+            if(mqttClientDict.get(client) != None):
+                mqttClientDict.pop(client)
+        time.sleep( 0.0001 )
+#def sendEventDataToMQTT():
+
+#Method to send mqtt data
+def startSubscribeCommandDataFromMQTT():
+        #Create MQTT client object and add to dict
+        client = mqtt.Client(MQTT_CLIENT_ID_BASE + "_SubscribeCommandData")
+        #Set events
+        client.on_connect = connectSubscribeCommandDataCallback
+        client.on_message = subscribeCommandDataCallback
+        #Set username and pasword if authentication is required
+        if MQTT_AUTHENTICATION:
+            client.username_pw_set(username=MQTT_BROKER_USER,password=MQTT_BROKER_PASSWORD)
+        #Connect to the broker
+        client.connect(MQTT_BROKER_IP)            
+        client.loop_forever()
+#def startSubscribeCommandDataFromMQTT():
 
 #-----------------Main program---------------------------
 if __name__ == "__main__":
@@ -186,17 +263,17 @@ if __name__ == "__main__":
 
     cliArgs = cliArgParser.parse_args()
     loglevel = cliArgs.log
-    logging.basicConfig(level=loglevel.upper())
+    logging.basicConfig(level=loglevel)
 
-    sendMQTTDataThread = Thread(target=sendMQTTData)
-    sendMQTTDataThread.start()
-    eventSubscribeThread = Thread(target=gardenaEventSubscribeTask)
-    eventSubscribeThread.start()
+    sendEventDataToMQTTThread = Thread(target=publishEventDataToMQTT)
+    sendEventDataToMQTTThread.start()
+    sendEventDataToMQTTThread = Thread(target=startSubscribeCommandDataFromMQTT)
+    sendEventDataToMQTTThread.start()
 
     while True:
         random = Random()
-        publishQueue.put(EventData(random.randint(10,100), random.randint(50,200)))
-        publishQueue.put(EventData(random.randint(10,100), random.randint(50,200)))
-        publishQueue.put(EventData(random.randint(10,100), random.randint(50,200)))
+        publishEventDataQueue.put(EventData("Device{0}".format(random.randint(20,50)), random.randint(10,100), random.randint(50,200)))
+        publishEventDataQueue.put(EventData("Device{0}".format(random.randint(20,50)),random.randint(10,100), random.randint(50,200)))
+        publishEventDataQueue.put(EventData("Device{0}".format(random.randint(20,50)),random.randint(10,100), random.randint(50,200)))
 
         time.sleep(10)
