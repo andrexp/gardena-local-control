4e29a157a43c20acd61dcbff569b7074035eec60
Andre Jendrysseck <ajendry@gwdg.de>
2023-04-20 22:06:41 +0200

    modify JSON parser

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 MQTT_BROKER_IP = '192.168.178.254'		                            # IP address of MQTT Broker
 MQTT_BROKER_PORT = 1883					                        # Port of MQTT Broker
-MQTT_AUTHENTICATION = True                                      # Mqtt authentication
+MQTT_AUTHENTICATION = False                                      # Mqtt authentication
 MQTT_BROKER_USER = ''			                                # Mqtt User name
 MQTT_BROKER_PASSWORD = ''			                            # Mqtt password
 MQTT_TOPIC_BASE = "GatewayName/DeviceAddress"                   # Base topic
